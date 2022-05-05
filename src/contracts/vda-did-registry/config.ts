317dea85439d62519e22cef82421900a0fe3f0ee
Alex <trust2ray@gmail.com>
2022-05-05 15:25:37 +0900

    Before add Web3
    Parse Parameters & Function name
    
    Next: Add Web3 contract interaction

--- src/contracts/vda-did-registry/config.ts
+++ src/contracts/vda-did-registry/config.ts
@@ -1,20 +1,21 @@
 
-export default class Config {
+class Config {
 
-
-    customParams(paramName, paramData) {
-        switch (paramName) {
-            case 'bulkAdd':
-                // @todo, custom stuff for bulk Add that changes paramData
-        }
+    customParams(paramName: any, paramData: any): Promise<any> {
+        // switch (paramName) {
+        //     case 'bulkAdd':
+        //         // @todo, custom stuff for bulk Add that changes paramData
+        // }
 
         return paramData
     }
 
-    aysnc isRequestValid(req) {
+    async isRequestValid(req: any): Promise<boolean> {
         // @todo: validate the request (ie: authentication, valid user-agent etc.)
 
         return true
     }
 
 }
+
+export default new Config()
--- src/genericController.ts
+++ src/genericController.ts
@@ -16,9 +16,15 @@ export default class GenericController {
     public static async contract(req: Request, res: Response) {
         const { contract, method } = req.params
 
+        // console.log("GenericContoller contract = ", contract)
+        // console.log("GenericContoller method = ", method)
+
         // @todo: try / catch to check if invalid contract specified
         const contractJson = require(`./contracts/${contract}/abi.json`)
-        const config = require(`./contracts/${contract}/config`)
+        // const config = require(`./contracts/${contract}/config`)
+        const config = (await import(`./contracts/${contract}/config`)).default
+
+        // console.log("GenericController config = ", config)
 
         const isValid = await config.isRequestValid(req)
         if (!isValid) {
@@ -29,6 +35,8 @@ export default class GenericController {
                 }
             })
         }
+
+        // console.log("GenericController", "Valid")
 
         const abi = contractJson.abi
         let abiMethod: any = false
@@ -48,23 +56,37 @@ export default class GenericController {
             })
         }
 
-        console.log("Found abiMethod definition", abiMethod)
-        console.log(req.body)
+        // console.log("Found abiMethod definition", abiMethod)
+        // console.log(req.body)
 
         // Loop through all the parameters and convert to the correct type
         const finalParams: any = {}
         abiMethod.inputs.forEach((param: any) => {
-            console.log(param)
+            // console.log(param)
+            let paramData : any
             switch (param.type) {
                 // @todo: convert types from string to correct type
-                case 'address':
-                case 'bytes32':
-                default:
-                    finalParams[param.name] = req.body[param.name]
+                // case 'address':{
+                //     break;
+                // }
+                // case 'bytes32':{
+                //     break;
+                // }
+                case 'tuple[]':{
+                    // console.log("Param - Tuple", param)
+                    paramData = JSON.parse(req.body[param.name]);
+                    break;                    
+                }
+                default: {
+                    paramData = req.body[param.name]
+                    break;
+                }
+                    
             }
-
-            finalParams[param.name] = config.customParams(param.name, req.body[param.name])
+            finalParams[param.name] = config.customParams(param.name, paramData)
         })
+
+        // console.log("Params: ", finalParams)
 
         // @todo: actually call the smart contract
 
