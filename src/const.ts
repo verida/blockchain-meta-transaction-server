4d799b069c5db7bbe497257b9d616f4bb0196e6f
Alex J <trust2ray@gmail.com>
2023-03-01 23:24:49 -0800

    Contract address updated and tested (#40)

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 // TestNet
 export const CONTRACT_ADDRESS_TESTNET : Record<string, string> = {
-    "VeridaDIDRegistry": "0x7347E3C6b8Fc38B2fb4a4f72E68953F0A4b899b0",
+    "VeridaDIDRegistry": "0x6905AD01eA5Bfb2fa651F9a2DF9659A7Cad8752a",
     "NameRegistry": "0x666F81e59082ccD7C8737f84e1C232a982043140",
     "SBT": "0x7201189556bAF5B58b74FDf99C1957A064B87548",
     "VeridaDIDLinkage": "0xBC50ba3FcC1e7FE97dcaddBb42630e8273589e6f",
